bda8f548d73bea79ddc5d26a6b546358b0484259
Saidfatah <said_designer@outlook.com>
2023-05-01 13:49:04 +0100

    added image and some more styling to itemDetail view

--- iDine/ItemDetail.swift
+++ iDine/ItemDetail.swift
@@ -11,14 +11,34 @@ struct ItemDetail: View {
     var item:MenuItem
     var body: some View {
         VStack{
-            Image(item.mainImage)
+            ZStack(alignment: .bottomTrailing){
+                Image(item.mainImage)
+                    .resizable()
+                    .scaledToFit()
+
+                Text("By: $\(item.photoCredit)")
+                    .padding(4)
+                    .foregroundColor(.white)
+                    .background(.black)
+                    .font(.caption)
+                    .offset(x:-5,y:-5)
+            }
+            
+            
             Text(item.description)
-        }.navigationTitle(item.name)
+                .padding()
+            Spacer()
+        }
+        .navigationTitle(item.name)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 struct ItemDetail_Previews: PreviewProvider {
     static var previews: some View {
-        ItemDetail(item:MenuItem.example)
+        NavigationView{
+            ItemDetail(item:MenuItem.example)
+        }
+        
     }
 }
