ed479a74536c6589b845ba9f7038d1b3915339f1
Saidfatah <said_designer@outlook.com>
2023-05-01 12:15:40 +0100

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by said fatah on 1/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
